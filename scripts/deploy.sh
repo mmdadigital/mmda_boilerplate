88fa0a79da49112b1f3beeb1f447f51b2f5863ae
guilopes <guiajlopes@gmail.com>
2015-03-03 10:30:01 -0300

    Jenkins files for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
-source arg_getter.sh
+PARSED_OPTIONS=$(getopt -o hni:f:t: --long "help,name,id:,from:,to:" -- "$@")
+if [ $? -ne 0 ];
+then
+  exit 1
+fi
+eval set -- "$PARSED_OPTIONS"
+while true;
+do
+  case "$1" in
+
+    -h|--help)
+      echo "usage -n -i -f -t OR --name --id --from --to"
+      shift;;
+
+    -n|--name)
+      if [ -n "$2" ];
+      then
+        JOB_NAME=$2
+      fi
+      shift 2;;
+
+    -i|--id)
+      if [ -n "$2" ];
+      then
+        BUILD_ID=$2
+      fi
+      shift 2;;
+
+    -f|--from)
+      if [ -n "$2" ];
+      then
+        FROM=$2
+      fi
+      shift 2;;
+
+    -t|--to)
+      if [ -n "$2" ];
+      then
+        WORKDIR=$2
+      fi
+      shift 2;;
+
+    --)
+      shift
+      break;;
+  esac
+done
 
 # TODO:
 # Fazer backup do banco, fazer backup dos arquivos.
@@ -7,9 +53,9 @@ source arg_getter.sh
 mkdir -p $WORKDIR
 cd $WORKDIR
 if [[ -d LATEST && -f build_id ]] ; then
-  mkdir -p /tmp/builds
+  mkdir -p /tmp/jenkins_builds
   # Move a última build
-  mv LATEST /tmp/builds/`cat build_id`
+  mv LATEST /tmp/jenkins_builds/`cat build_id`
 fi
 
 cp -fr $FROM LATEST
@@ -25,22 +71,15 @@ ln -sf $WORKDIR/shared_files $WORKDIR/LATEST/docroot/sites/default/files
 # Aponta o arquivo settings.local.php pro arquivo de configurações compartilhado por todas as builds.
 ln -sf $WORKDIR/settings.local.php $WORKDIR/LATEST/docroot/sites/default/settings.local.php
 
+# Cria pasta temporária
+mkdir -p $WORKDIR/LATEST/docroot/tmp
+
 # Ajusta as permissões
 chown -R $USER:$USER $WORKDIR
 chmod -R 777 $WORKDIR/shared_files
 
 # Ajustes pré-deploy do Drupal
 # Faz update do banco de dados, reverte todas as features e limpa o cache.
-cd $WORKDIR/LATEST/docroot
-drush -y dis devel
-drush -y dis views_ui
-drush -y dis context_ui
-drush -y dis update
-drush -y dis l10n_update
-drush -y dis dblog
-drush -y dis rules_ui
-drush -y dis update
-drush -y dis field_ui
-drush updb
-drush cc all
-drush fra -y
+cd $WORKDIR/LATEST
+phing -buildfile $FROM/build.xml
+# drush build -y
